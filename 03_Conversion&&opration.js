cf2a9dbd9226b36e2689728adf6aa21b030872e2
Swap-sutar <118467297+Swap-sutar@users.noreply.github.com>
2023-12-29 10:03:35 +0530

    Type conversion int to string.

--- 03_Conversion&&opration.js
+++ 03_Conversion&&opration.js
@@ -1,1 +1,39 @@
-1.1
+//Type conversion int to string.
+let someNumber = 33;
+let StringNumber = String(someNumber);
+console.log(StringNumber);
+console.log(typeof(StringNumber));
+
+
+//Type conversion string to int.
+let str = "33"
+let num = Number(str);
+console.log(num);
+console.log(typeof(num));
+
+
+//Type conversion str to boolean .
+// it's shows output in true or false.
+
+let name = "swapnil";
+let boolname = Boolean(name);
+console.log(boolname);
+console.log(typeof(boolname));
+
+
+//Type conversion num to boolean.
+// it's shows output in true or false.
+
+let num1 = 33;
+let boolNum = Boolean(num1);
+console.log(boolNum);
+console.log(typeof(boolNum));
+
+
+//Type conversion empty string to boolean.
+// it's shows output in true or false.
+
+let emptyStr = "";
+let bool = Boolean(emptyStr);
+console.log(bool);
+console.log(typeof(bool));
